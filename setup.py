00bbdd0bfdde2f5cd94fc375f5472c330a32aa6f
Ozer Ozkahraman <ozero@kth.se>
2023-09-19 18:19:42 +0200

    Working package

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import os, glob
 
 package_name = 'smarc_nodered'
 
@@ -7,9 +8,10 @@ setup(
     version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob.glob('config/*')),
+        (os.path.join('share', package_name, 'launch'), glob.glob('launch/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
